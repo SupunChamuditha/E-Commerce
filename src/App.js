b3a114053d9cd5ce32262899baa22386e35b47cc
A C S C WIJAYARTHNE <KUDSE222F-062@student.nibm.lk>
2024-05-04 10:16:09 +0530

    Add routes to pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import LoginSignup from './Pages/LoginSignup';
+import Cart from './Pages/Cart';
+import Shop from './Pages/Shop';
+
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/product' element={<Product/>}>
+         <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}></Route>
+        <Route path='/login' element={<LoginSignup/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
+
   );
 }
 
