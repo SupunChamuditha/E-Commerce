40516ebbed138df2ef653f59cd5f672d6d41598f
A C S C WIJAYARTHNE <KUDSE222F-062@student.nibm.lk>
2024-05-08 09:42:20 +0530

    Description Box added to pages

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+          <div className="descriptionbox-nav-box">Description</div>
+          <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+    </div>
+          <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet voluptas corporis praesentium fuga at, quis, beatae voluptate molestiae deserunt provident mollitia, atque recusandae! Ipsum, iure laboriosam. Ipsa quibusdam quos blanditiis!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa vitae facilis sequi quis temporibus vel ratione? Labore, sit quis id tenetur in deserunt facilis architecto cumque maiores porro ipsam distinctio?</p>
+          </div>
+        
+    </div>
+  )
+}
+
+export default DescriptionBox
