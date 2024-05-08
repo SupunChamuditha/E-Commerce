3a5d272b5f3eeca6ed1162a47fc77372e5ab886b
A C S C WIJAYARTHNE <KUDSE222F-062@student.nibm.lk>
2024-05-08 11:20:12 +0530

    add to cart funtion added

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import CartItems from '../Components/CartItems/CartItems'
 
 function cart() {
   return (
-    <div>cart</div>
+    <div>
+      <CartItems/>
+    </div>
   )
 }
 
